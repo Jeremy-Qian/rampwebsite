36cf89cfd1df3270989aef786a71c94011190cda
Jeremy <jeremy@qianyongchangandJeremys-MacBook-Pro.local>
2025-03-11 10:33:06 +0800

    Updated thinking spinner

--- app.py
+++ app.py
@@ -145,7 +145,9 @@ def main():
             if prompt := st.chat_input("Message JereChat..."):
                 messages.chat_message(st.session_state.user).write(prompt)
                 #messages.chat_message("user", avatar=":material/person:").write(prompt)
-                messages.chat_message("jerechat", avatar="icon_small.png").write(f"{model.generate_response(prompt)}")
+                with st.spinner("Thinking..."):
+                    resp = model.generate_response(prompt)
+                messages.chat_message("jerechat", avatar="icon_small.png").write(f"{resp}")
     elif st.session_state.user and st.session_state.role == "Tester":
         with st.sidebar:
             messages = st.container(height=300)
