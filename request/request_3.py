4e3a8bae04c386585b2483b849950eb8d0ad0785
Jeremy-Qian <917592095@qq.com>
2025-03-21 19:26:19 +0800

    docs(request_3.py): 调整评论部分的标题层级和格式
    调整了评论部分的标题层级和格式，使其更加一致和易读。主要修改包括将部分标题从三级改为四级，并调整了部分文本的缩进和格式。

--- request/request_3.py
+++ request/request_3.py
@@ -5,12 +5,12 @@ st.header("Website Comments")
 #Link to GitHub issues
 st.markdown("""
 ##### Is there any bugs?
-### Very serious?  
-###### Please report it to us!  
-##### Have any suggestions?  
-###### Please let us know!  
-##### Have any questions?  
-###### Ask Jeremy!  
+#### Very serious?  
+Please report it to us!  
+###### Have any suggestions?  
+Please let us know!  
+###### Have any questions?  
+Ask Jeremy!  
 You can visit our GitHub repository to see the [source code](https://github.com/Jeremy-Qian/rampwebsite).  
 Or you can contact us by email: (Joshua's e-mail)  
 You can start an issue here:  
