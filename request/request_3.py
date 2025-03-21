3671768ca04812686db32eab0dd373cbb0219d0a
Jeremy-Qian <917592095@qq.com>
2025-03-21 19:24:09 +0800

    docs: 更新网站评论部分的Markdown格式
    更新了request_3.py文件中的Markdown格式，添加了新的问题类型“有任何问题？”并指定了联系人Jeremy。同时调整了其他文本的格式，使其更加清晰和一致。

--- request/request_3.py
+++ request/request_3.py
@@ -4,15 +4,17 @@ st.header("Website Comments")
 
 #Link to GitHub issues
 st.markdown("""
-###### Is there any bugs?
-###Very serious?
-*Please report it to us!*
-###### Have any suggestions?
-*Please let us know!*
-You can visit our GitHub repository to see the [source code](https://github.com/Jeremy-Qian/rampwebsite).
-Or you can contact us by email: (Joshua's e-mail)
-You can start an issue here:
-[GitHub Issues](https://github.com/Jeremy-Qian/rampwebsite/issues)
+##### Is there any bugs?
+### Very serious?  
+###### Please report it to us!  
+##### Have any suggestions?  
+###### Please let us know!  
+##### Have any questions?  
+###### Ask Jeremy!  
+You can visit our GitHub repository to see the [source code](https://github.com/Jeremy-Qian/rampwebsite).  
+Or you can contact us by email: (Joshua's e-mail)  
+You can start an issue here:  
+[GitHub Issues](https://github.com/Jeremy-Qian/rampwebsite/issues)  
 """)
 
 
