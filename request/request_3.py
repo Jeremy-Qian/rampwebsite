7959f6ffb3dbdfcdcd847543130e21b342fbc669
Jeremy-Qian <917592095@qq.com>
2025-03-21 19:29:08 +0800

    docs: 优化网页评论部分的文档格式和内容
    重新组织并简化了网页评论部分的文档内容，使其更具可读性和一致性。更新了链接格式和文本结构，以便用户更清晰地了解如何报告问题或提出建议。

--- request/request_3.py
+++ request/request_3.py
@@ -2,19 +2,20 @@ import streamlit as st
 import streamlit_chat as st_chat
 st.header("Website Comments")
 
-#Link to GitHub issues
+# Link to GitHub issues
 st.markdown("""
-##### Is there any bugs?
-#### Very serious?  
-Please report it to us!  
-###### Have any suggestions?  
-Please let us know!  
-###### Have any questions?  
-Ask Jeremy!  
-You can visit our GitHub repository to see the [source code](https://github.com/Jeremy-Qian/rampwebsite).  
-Or you can contact us by email: (Joshua's e-mail)  
-You can start an issue here:  
-[GitHub Issues](https://github.com/Jeremy-Qian/rampwebsite/issues)  
+### Report Bugs or Suggestions！
+
+If you encounter any bugs or have suggestions for improvements, please feel free to [report them on GitHub](URL_ADDRESSIf you encounter any bugs or have suggestions for improvements, please feel free to [report them on GitHub](https://github.com/Jeremy-Qian/rampwebsite/issues).
+
+- **Found a bug?** If the issue is serious, please report it to us!  
+- **Have suggestions?** We'd love to hear from you!  
+- **Have questions?** Ask Jeremy!  
+
+You can view the [source code](https://github.com/Jeremy-Qian/rampwebsite) on our GitHub repository.  
+Or contact us via email: (Joshua's e-mail)  
+
+To start an issue, click [here](https://github.com/Jeremy-Qian/rampwebsite/issues).  
 """)
 
 
