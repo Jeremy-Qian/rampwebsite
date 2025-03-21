443c11dfefe85ed31de994957056679388449c12
Jeremy-Qian <917592095@qq.com>
2025-03-21 19:17:53 +0800

    docs(request_3.py): 添加GitHub问题和建议的链接
    在页面中添加了指向GitHub仓库和问题的链接，方便用户报告错误或提出建议

--- request/request_3.py
+++ request/request_3.py
@@ -1,3 +1,18 @@
 import streamlit as st
 import streamlit_chat as st_chat
 st.header("Website Comments")
+
+#Link to GitHub issues
+st.markdown("""
+###### Is there any bugs?
+###Very serious?
+*Please report it to us!*
+###### Have any suggestions?
+*Please let us know!*
+You can visit our GitHub repository to see the [source code](https://github.com/Jeremy-Qian/rampwebsite).
+Or you can contact us by email: (Joshua's e-mail)
+You can start an issue here:
+[GitHub Issues](https://github.com/Jeremy-Qian/rampwebsite/issues)
+""")
+
+
