525023f33ca4f1256a299a6ff5475589a23e1bb0
Jeremy-Qian <917592095@qq.com>
2025-03-22 18:31:44 +0800

    docs(request_3.py): 更新网站评论部分的文档内容
    更新了网站评论部分的文档内容，包括版本号、最后更新日期、报告问题和联系方式的详细信息。添加了GitHub登录提示，以便用户更好地了解如何提交问题或建议。

--- request/request_3.py
+++ request/request_3.py
@@ -5,18 +5,25 @@ st.header("Website Comments")
 # Link to GitHub issues
 st.markdown("""
 ### Report Bugs or Suggestions!  
-> Change date: 2023-08-25
-> Version: 1.0.1
+> Last renewed date: 2023-08-25
+> Version: 1.0.2
 > This website is still under development. If you encounter any bugs or have suggestions for improvements, please feel free to [report them on GitHub](https://github.com/Jeremy-Qian/rampwebsite/issues).
+> You can view the [source code](https://github.com/Jeremy-Qian/rampwebsite) on our GitHub repository.  
 
+##### Report Issues
 - **Found a bug?** If the issue is serious, please report it to us!  
 - **Have suggestions?** We'd love to hear from you!  
-- **Have questions?** Ask Jeremy!  
+- **Have questions?** Ask Jeremy!    
+  
+##### Contact Us  
+You can contact us via email: 
+- 917592095@qq.com  
+- joshuabatluck@gmail.com  
 
-You can view the [source code](https://github.com/Jeremy-Qian/rampwebsite) on our GitHub repository.  
-Or contact us via email: (Joshua's e-mail)  
-
+Or start an issue on GitHub.
 To start an issue, click [here](https://github.com/Jeremy-Qian/rampwebsite/issues).  
+Note:   
+:red[You have to login to GitHub to start an issue. If you don't have a GitHub account, you can create one for free. Or you can just send an email to us. Thank you!]
 """)
 
 
