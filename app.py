93c2eb504fdf8ed906403a797a458c3d87155dec
Jeremy-Qian <917592095@qq.com>
2025-04-07 20:10:25 +0800

    fix: 修正网页链接格式
    修复了网页链接中缺少 `https://` 的问题，确保链接可以正确跳转

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ st.set_page_config(
 )
 
 st.markdown(":red-badge[A big problem] This page has a BIG problem. Wait for a few days/weeks...\
-            Test new website: [Click here](pleaseusethiswebsitedirectlywithoutlogginginyouarearamper.streamlit.app)")
+            Test new website: [Click here](https://pleaseusethiswebsitedirectlywithoutlogginginyouarearamper.streamlit.app)")
