9fee1e1d70dcf414b2c8c6e3e01ee435d441e180
Jeremy-Qian <jeremytheking2013@gmail.com>
2025-06-01 16:34:39 +0800

    Hide even better and fix bug

--- app.py
+++ app.py
@@ -2,10 +2,8 @@ import streamlit as st
 st.markdown(
     """
     <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
+    MainMenuBar {
+        visibility: hidden;
     }
     </style>
     """,
