7d0739a68c92681ad5c3da8edee1abfdfa840871
Jeremy-Qian <jeremytheking2013@gmail.com>
2025-07-28 10:18:47 +0800

    Update groups and add theming

--- groups.py
+++ groups.py
@@ -3,14 +3,5 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.write("## Group")
-st.write("""
-Thought for 10 seconds  
-> Now, the user said, 'Good bye !'.   
-> What should I say?   
-> Should I say 'Good bye !'? That got a score of 0.  
-> Or 'Good .'? That got a score of 0.  
-> Or 'Good boy !'? That got a score of 0.  
-> Or maybe 'Good bye !' or 'Good bye !'? Those got scores of 0 and 0.  
-Good .  
-""")
+st.write("## Groups")
+st.warning("This is still under development.")
